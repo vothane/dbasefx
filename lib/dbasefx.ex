9497bac419048709f0cca88f59b5e1fd6c7f503b
Thane Vo <vothane@gmail.com>
2015-10-14 01:52:45 +0000

    add Table module

--- lib/dbasefx.ex
+++ lib/dbasefx.ex
@@ -31,3 +31,15 @@ results_and_averages =
 ]
 
 end
+
+defmodule Table do
+  def new(columns, rows // []) do
+    %{:columns => columns, :rows => rows}
+  end
+
+  def insert(table, row) do
+    new_row = Enum.zip(Map.fetch(table, :columns), row)
+    %{table | :rows => List.insert_at(Map.fetch(:rows), -1, new_row)}
+  end
+end
+
