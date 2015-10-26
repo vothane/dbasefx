2e4cb4e36a445614363e74512c022976d63f504e
Thane Vo <vothane@gmail.com>
2015-10-26 23:26:53 +0000

    Enum.any? is too restrictive hard as is in where clause. Should be used in the higher-order function predicate.

--- lib/dbasefx.ex
+++ lib/dbasefx.ex
@@ -12,9 +12,7 @@ defmodule Dbasefx do
   end
 
   def where(table, predicate) do
-    result_rows =
-      Enum.filter(Map.get(table, :rows),
-        fn(row) -> Enum.any?(row, predicate) end)
+    result_rows = Enum.filter(Map.get(table, :rows), predicate)
     Table.new(Map.get(table, :columns), result_rows)
   end
 
@@ -27,26 +25,28 @@ defmodule Dbasefx do
   end
 
   def join(table, other_table) do
-    join_cols = HashSet.to_list(Set.intersection(Enum.into(Map.get(table, :columns), HashSet.new),
-                                                 Enum.into(Map.get(other_table, :columns), HashSet.new)))
-    right_cols = HashSet.to_list(Set.difference(Enum.into(Map.get(other_table, :columns), HashSet.new),
-                                Enum.into(Map.get(table, :columns), HashSet.new)))
+    join_cols = Set.intersection(Enum.into(Map.get(table, :columns), HashSet.new),
+                                 Enum.into(Map.get(other_table, :columns), HashSet.new))
+                |> HashSet.to_list
+    right_cols = Set.difference(Enum.into(Map.get(other_table, :columns), HashSet.new),
+                                Enum.into(Map.get(table, :columns), HashSet.new))
+                 |> HashSet.to_list
+
     join_table = Table.new(join_cols ++ right_cols)
-    reduce_fn =
-      fn(row, table) ->
-        is_join? =
-          fn(other_row) ->
-            IO.puts "--------> #{row}"
-            Enum.all?(join_cols, fn(col) -> other_row[col] == row[col] end)
-          end
-        other_rows = Map.get(Dbasefx.where(other_table, is_join?), :rows)
-        Enum.reduce(other_rows, join_table,
-          fn(r, t) ->
-            new_row = row ++ r
-            %{t | :rows => List.insert_at(Map.get(t, :rows), -1, new_row)}
-          end)
+
+    for row <- Map.get(table, :rows) do
+      is_join? = fn(other_row) ->
+        IO.inspect join_cols
+        IO.inspect row["Pitch"]
+        a = {k, v} = other_row
+        IO.inspect a["Pitch"]
+        Enum.all?(for col <- join_cols, do: Enum.into(other_row, %{})[col] == row[col]) end
+      other_rows = Map.get(Dbasefx.where(other_table, is_join?), :rows)
+      for other_row <- other_rows do
+        new_row = for c <- Map.get(table, :columns), do: row[c] ++ for c <- right_cols, do: other_row[c]
+        %{join_table | :rows => List.insert_at(Map.get(join_table, :rows), -1, new_row)}
       end
-    Enum.reduce(Map.get(table, :columns), join_table, reduce_fn)
+    end
   end
 end
 
