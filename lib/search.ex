4765ffd99ed292c06b37a83a77bba0c634123329
Thane Vo <vothane@gmail.com>
2016-04-17 01:24:21 +0000

    reply with tables instead of query results

--- lib/search.ex
+++ lib/search.ex
@@ -2,29 +2,18 @@ defmodule Search do
   use GenServer 
   import Dbasefx
 
-  def init(_) do 
-    {:ok, Map.new} 
-  end 
-
-  def handle_cast({:put, player, player_url}, state) do 
-    table_map = %{:trajectory_movement => player_url,
-                  :pitch_usage => "#{player_url}&var=usage",
-                  :pitch_outcomes => "#{player_url}&var=po",
-                  :sabermetric_outcomes => "#{player_url}&var=so",
-                  :results_averages => "#{player_url}&var=ra"}
-    primary_keys = ["Pitch Type", "Count"]            
-    db = Enum.reduce(table_map, %{}, fn({table_name, url}, m) -> 
-                                       Map.put(m, table_name, Webscraper.build_table(url, primary_keys))
-                                     end)  
-    {:noreply, Map.put(state, player, db)} 
-  end 
-
-  def handle_call({:get, player, query}, _, state) do
-    %{:trajectory_movement => trajectory_movement,
-      :pitch_usage => pitch_usage,
-      :pitch_outcomes => pitch_outcomes,
-      :sabermetric_outcomes => sabermetric_outcomes,
-      :results_averagee => results_averages} = Map.get(state, player)
-    {:reply, query.(), state} 
+  def handle_call({:get, player, player_url}, _, state) do
+    tables = case Map.has_key?(state, player) do
+               false -> table_map = %{:trajectory_movement => player_url,
+                                      :pitch_outcomes => "#{player_url}&var=po",
+                                      :sabermetric_outcomes => "#{player_url}&var=so",
+                                      :results_averages => "#{player_url}&var=ra"}
+                        primary_keys = ["Pitch Type", "Count"]            
+                        Enum.reduce(table_map, %{}, fn({table_name, url}, m) -> 
+                                                      Map.put(m, table_name, Webscraper.build_table(url, primary_keys))
+                                                    end)  
+               true -> Map.get(state, player)
+             end
+    {:reply, tables, state} 
   end 
 end
---
+++ test/search_test.exs
@@ -0,0 +1,22 @@
+defmodule SearchTest do
+  use ExUnit.Case
+  use GenServer
+  doctest Search
+
+  test "search" do
+    {:ok, pid} = GenServer.start_link(Search, Map.new)
+    player = "David Price"
+    player_url = "http://www.brooksbaseball.net/tabs.php?player=456034"
+     %{:trajectory_movement => trajectory_movement,
+       :pitch_outcomes => pitch_outcomes,
+       :sabermetric_outcomes => sabermetric_outcomes,
+       :results_averages => results_averages} = GenServer.call(pid, {:get, player, player_url})
+    
+     query = Dbasefx.select(trajectory_movement, ["Pitch"])
+          |> Dbasefx.where(fn(row) -> Enum.any?(row, fn {k, v} -> {k, v} == {"Pitch", "Change"} end) end)
+     
+    result = Map.get(query, :rows)
+    assert result == []
+    
+  end
+end
