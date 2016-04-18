9d6ec56e7e3bebb9e3efdcc46766b3b2f0067bfa
Thane Vo <vothane@gmail.com>
2016-04-18 01:08:34 +0000

    fix pattern matching of table elements

--- test/search_test.exs
+++ test/search_test.exs
@@ -12,6 +12,7 @@ defmodule SearchTest do
        :sabermetric_outcomes => sabermetric_outcomes,
        :results_averages => results_averages} = GenServer.call(pid, {:get, player, player_url})
     
+     IO.inspect GenServer.call(pid, {:get, player, player_url})
      query = Dbasefx.select(trajectory_movement, ["Pitch"])
           |> Dbasefx.where(fn(row) -> Enum.any?(row, fn {k, v} -> {k, v} == {"Pitch", "Change"} end) end)
      
