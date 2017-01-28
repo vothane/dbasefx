7843308411db718b0b8a73eb70eec5db050a1dca
Thane Vo <vothane@gmail.com>
2017-01-28 03:00:54 +0000

    add more meaningful and complex query for WHERE clause

--- test/search_test.exs
+++ test/search_test.exs
@@ -17,12 +17,20 @@ defmodule SearchTest do
      IO.inspect sabermetric_outcomes
      IO.inspect results_averages
 
-     query = Dbasefx.select(trajectory_movement, ["Pitch Type"])
-          |> Dbasefx.where(fn(row) -> Enum.any?(row, fn {k, v} -> {k, v} == {"Pitch Type", "Change"} end) end)
+     query_change = Dbasefx.select(trajectory_movement, ["Pitch Type"])
+                 |> Dbasefx.where(fn(row) -> Enum.any?(row, fn {k, v} -> {k, v} == {"Pitch Type", "Change"} end) end)
      
-    result = Map.get(query, :rows)
+     result_change = Map.get(query_change, :rows)
 
-    assert result == [[{"Pitch Type", "Change"}]] # query if David Price has a changeup in repertoire, query indicates TRUE
+     assert result_change == [[{"Pitch Type", "Change"}]] # query if David Price has a changeup in repertoire, query indicates TRUE
     
+     query_quarter_of = Dbasefx.select(trajectory_movement, ["Pitch Type", "Freq"])
+                     |> Dbasefx.where(fn([pitch | freq]) -> elem(List.first(freq), 1) >= 25.0 end)
+     
+     result_quarter_of = Map.get(query_quarter_of, :rows)
+
+     # query to find all pitches that account for a quarter of his total pitch count percentage
+     # they are the fastball (4-seam) and sinker (2-seam)
+     assert result_quarter_of == [[{"Pitch Type", "Fourseam"}, {"Freq", 27.45}], [{"Pitch Type", "Sinker"}, {"Freq", 33.85}]]
   end
 end
